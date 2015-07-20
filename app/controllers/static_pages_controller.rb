6d9cd109b7920479bb898fed7b713a85d83d4717
Caitlin Keck <caitlin.keck@gmail.com>
2015-07-19 23:30:50 -0400

    Added background, about section, miscellaneous css

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,12 @@
 class StaticPagesController < ApplicationController
   def home
+    @home_page = true
   end
 
   def help
   end
 
   def about
+    @home_page2 = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
-
   get 'static_pages/help'
-
   get 'static_pages/about'
 
   root to: 'static_pages#home'
